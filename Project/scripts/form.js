a0d5e85ed615e82cead1ba4e6e00308380047400
wealthiness301 <osaigbovowealth3@gmail.com>
2025-10-09 10:04:19 +0100

    change

---
+++ Project/scripts/form.js
@@ -0,0 +1,22 @@
+const products = [
+    { id: "fc-1888", name: "Flux Capacitor" },
+    { id: "fc-2050", name: "Power Laces" },
+    { id: "fc-1987", name: "Time Circuits" },
+    { id: "ac-2000", name: "Low Voltage Reactor" },
+    { id: "jj-1969", name: "Warp Equalizer"}
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productSelect = document.getElementById("productName");
+    //Populate the product options dynamically
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+
+    //Show the last modification date
+
+    document.getElementById("lastModified").textContent = document.lastModified;
+});
